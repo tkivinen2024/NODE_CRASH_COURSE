122b282d2a5fd8256f805a64d07fb6eb6c28a229
Timo Kivinen <Timo.Kivinen@student.lut.fi>
2024-03-21 23:38:23 +0200

    Video: 22:44

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-// console.log('Hello From Node.js...');
-const person = require('./person');
+// -----------------------------------------------------------------
+// --- 21.3.2024  Timo Kivinen
+// ---
+// ---
+// ---
+// --- Video: 22:44 (https://www.youtube.com/watch?v=fBNz5xF-Kx4)
+// -----------------------------------------------------------------
 
-console.log(person.name);
+// 1 console.log('Hello From Node.js...');
+//const person = require('./person');
+const Person = require('./person');
+const person1 = new Person('John Doe', 30);
+
+//console.log(person.name);
+
+person1.greeting();
--- person.js
+++ person.js
@@ -1,8 +1,22 @@
-const person = {
-    name: 'John Doe',
-    age: 30
+//const person = {
+//    name: 'John Doe',
+//    age: 30
+// --- Module Wrapper function
+//(function (exports, require, module, __filename, __dirname) {
+//})
+// console.log(__dirname, __filename);
+
+
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    greeting() {
+        console.log(`My name is ${this.name} and I am ${this.age}`);
+    }
 }
 
-module.exports = person;
-
+//module.exports = person;
+module.exports = Person;
 
